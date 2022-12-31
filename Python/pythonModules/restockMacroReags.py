6ac1252304772e5d97bc2b1bc7f9343d717e5a42
ted182 <tedesco182@gmail.com>
2022-12-31 11:35:55 -0300

    Update restockMacroReags.py

--- Python/pythonModules/restockMacroReags.py
+++ Python/pythonModules/restockMacroReags.py
@@ -1,31 +1,67 @@
 from stealth import FindType, FindQuantity, FindFullQuantity, GetQuantity, MoveItem, Backpack, Dead
+from datetime import datetime, timedelta
+
 from pythonModules.aguardar import *
 from pythonModules.openBank import *
 from pythonModules.openContainer import *
 
 
-def restockMacroReags(_bagBankReags, _reagsDict):
+def reagBagAmount(_type):
+    return GetQuantity( FindType(_type, Backpack()) )
+
+def checkBankResources(_bagBankReags, _reagsDict):
+
+    checkTimes = 3  #roda a rotina x vezes para garantir que não deu erro
+    attempts = 0
+    err = 0
+
+    while attempts <= checkTimes - 1 :
+
+        openBank()
+        openContainer(_bagBankReags)
+
+        for item in _reagsDict:
+            aguardar(100)
+            if FindType(item['type'], _bagBankReags) > 0:                    
+                item['bankAmount'] = FindFullQuantity()
+            else:
+                #print('erro-01')
+                err += 1
+        
+        attempts += 1
+
+    if err >= checkTimes:
+        return False
     
+    return True
+
+def printBankResources(_reagsDict):
+    for item in _reagsDict:
+        print( item['name'] + ' disponíveis: ' + str( item['bankAmount'] ) )
+
+
+def restockMacroReags(_bagBankReags, _reagsDict):   
+
     if Dead():
         return False
     
-    openBank()
-    openContainer(_bagBankReags)
-
+    if not checkBankResources(_bagBankReags, _reagsDict):
+        print( '[restock-reags] -> sem reags na bag de restock do banco...' )
+        exit()
+    
+    timeout = datetime.now() + timedelta(seconds=300)
+    
     for item in _reagsDict:
-        
-        aguardar(100)
-
-        if FindType(item['type'], _bagBankReags) > 0:
             
-            item['bankAmount'] = FindFullQuantity()
-            print( item['name'] + ' disponíveis: ' + str(FindFullQuantity()) )
+        aguardar(250)
 
-            while GetQuantity( FindType(item['type'], Backpack()) ) < item['bagAmount']:
-                qnt = item['bagAmount'] - FindQuantity()
-                MoveItem( FindType(item['type'], _bagBankReags) , qnt, Backpack(), 0, 0, 0)
-                aguardar(250)    
-
-        else:
-            print( item['name'] + ' acabou na bag de restock do banco...' )
-            exit()
+        while reagBagAmount(item['type']) < item['bagAmount']:
+            if (datetime.now() > timeout):
+                break
+            openBank()
+            openContainer(_bagBankReags)
+            qnt = item['bagAmount'] - FindQuantity()
+            MoveItem( FindType(item['type'], _bagBankReags) , qnt, Backpack(), 0, 0, 0)
+            aguardar(250) 
+    
+    printBankResources(_reagsDict)
